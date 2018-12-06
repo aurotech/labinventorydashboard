e8beec4232b5855925053b866dc21edf434ef2b4
Keshav885 <kesava.paruchuri@aurotechcorp.com>
2018-12-06 14:42:25 -0500

    added homepage ./ in package.json

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b376f2e3a62375a30b4fed371c3eed7f.js"
+  "./precache-manifest.0ea8c719c82c170f3f77584fb0d5a1c3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
